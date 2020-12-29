209e4814c7bf9d63d22dfd9d4fcb488000ab4caa
Yulia Nefodova <36659197+YuliaNefodova@users.noreply.github.com>
2020-12-29 20:42:40 +0200

    Add files via upload

---
+++ src/com/company/MathTask.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.lang.Math;
+
+public class MathTask {
+
+    public String getProjectileFlightDistance(double v, double a){
+
+        double resultDegrees = Math.pow(v,2) * (Math.sin(2 * Math.toDegrees(a)) )/ 9.81;
+        double resultRadians = Math.pow(v,2) * (Math.sin(2 * Math.toRadians(a)) )/ 9.81;
+
+        String result = "In degrees " + String.format("%.2f",resultDegrees) + "\n" + "In radians " + String.format("%.2f",resultRadians) ;
+
+        return result;
+    }
+
+    public double getDistanceThroughTime(double v1, double v2, double s, double t){
+
+       double result = (v1 + v2) * t + s;
+
+       return result;
+    }
+
+    public int getDomainOwnership(double x, double y){
+
+        int result;
+
+        if ( Math.abs(x - y )>= 0 & Math.abs( 3*x - 2*y - 2 )<= 0 ) {
+            result = 1;
+        }
+        else
+            result = 0;
+
+      return result;
+    }
+
+    public double evaluateExpression(double x){
+
+        double result = (6 * Math.log(Math.cbrt(Math.exp(x + 1) + 2 * Math.exp(x) * Math.cos(x))))/
+                (Math.log(x - Math.exp(x + 1) * Math.sin(x)))
+                + Math.abs(Math.cos(x) / Math.exp(Math.sin(x)));
+
+        return result;
+    }
+
+}
